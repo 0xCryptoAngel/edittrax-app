1e9e69bbf1a4346cddd582c3caea2d3f85dca742
bryanballidev <bryanballidev@gmail.com>
2022-10-11 00:50:13 -0700

    completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,25 +1,21 @@
 
 
-import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
-import square_download_mechanism from '../assets/square_download_mechanism.jpg'
-import square_boxxed from "../assets/square_boxxed.png";
-import square_burnt from '../assets/square_burnt.jpg'
-import square_alpha_test from "../assets/square_alpha_test.png";
+import React from "react";
 
-import unlockable_burnt from '../assets/unlockable_burnt.jpg'
-import unlockable_boxxed from "../assets/unlockable_boxxed.png";
-import unlockable_alpha_test from '../assets/unlockable_alpha_test.jpg'
-import unlockable_download_mechanism from "../assets/unlockable_download_mechanism.png";
+type WalletProps = {
+  square: any,
+  unlockable: any,
+  title: string | undefined,
+};
 
-const Content = (props:any) => {
-
+const Content = (props:WalletProps) => {
   return (
     <section className="bg-white flex flex-col  items-center gap-8 px-8 py-12 mb-4 mx-4 md:mb-0 md:pb-0 md:mx-0">
       <div className="flex flex-col items-center gap-8 md:flex-row md:w-full md:gap-8 md:items-end">
-      <img src={`${props.square}`} alt="rotating head" className="w-52"/>
+      <img src={props.square} alt="rotating head" className="w-52"/>
       <div className="flex flex-col items-center md:items-start md:justify-end md:h-full">
         <div className="font-mathias text-5xl font-bold">BAI-EE</div>
-        <div className="font-mathias text-5xl font-bold text-gray-300">"ACID BEACH"</div>
+        <div className="font-mathias text-5xl font-bold text-gray-300">"{props.title}"</div>
         <div className="flex flex-wrap gap-4 pt-6">
           <button className="bg-gray-300 rounded-full px-4 py-1 text-xxs">DOWNLOAD</button>
           <button className="bg-gray-300 rounded-full px-4 py-1 text-xxs">OPENSOURCE</button>
@@ -40,7 +36,7 @@ const Content = (props:any) => {
           <h1 className="font-mathias text-center">DESCRIPTION</h1>
           <p>Bai-ee's "Acid Beach" (ET001) is a Detroit influenced House track set to 808 drums, 303 bassline and modular driven synth. Released as a full featured (ALPHA) test this interactive music collectible features an integrated looper and token gated download mechanism.</p>
           </div>
-          <img src={`${props.unlockable}`} alt="edittrax process"/>
+          <img src={props.unlockable} alt="edittrax process"/>
           <div>
           <h1 className="font-mathias text-center">RIGHTS</h1>
           <p>As the owner of this collectible, you are granted the right to perform its downloadable content in public. This includes playing in mix-tapes, online streams, social feeds, and live performances. You do NOT have the right to repackage downloaded content for resale or distribution. Assume NO other rights of ownership.</p>
--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -7,6 +7,15 @@ import { useParams } from "react-router-dom";
 
 import load from '../assets/load.gif'
 
+import square_download_mechanism from '../assets/square_download_mechanism.jpg'
+import square_boxxed from "../assets/square_boxxed.jpg";
+import square_burnt from '../assets/square_burnt.jpg'
+import square_alpha_test from "../assets/square_alpha_test.jpg";
+
+import unlockable_burnt from '../assets/unlockable_burnt.png'
+import unlockable_boxxed from "../assets/unlockable_boxxed.png";
+import unlockable_alpha_test from '../assets/unlockable_alpha_test.png'
+import unlockable_download_mechanism from "../assets/unlockable_download_mechanism.png";
 
 
 
@@ -34,26 +43,30 @@ const navbarMenu = [
   {
     player: "mechanism",
     imageUrl: "https://ipfs.io/ipfs/QmSqWNSGuPCfZQ8uJwhi94aXZDXRDbP5a75MtR7YBgDYDS/?creator=tz1cpiv1qgjzNsMbqHYyUdH8XzZ672bjdm2E&objkt=781874&viewer=",
-    square: 'square_download_mechanism',
-    unlockable: 'unlockable_download_mechanism',
+    square: square_download_mechanism,
+    unlockable: unlockable_download_mechanism,
+    title: 'acid beach (editteraxdug)'
   },
   {
     player: "burnt",
     imageUrl: "https://ipfs.io/ipfs/QmTvcnqnBjcjMQ1gDoG5jSvap7id7t2nhvQLmLiCRqSFqq/?creator=tz1cpiv1qgjzNsMbqHYyUdH8XzZ672bjdm2E&objkt=781875&viewer=",
-    square: 'square_burnt',
-    unlockable: 'unlockable_burnt',
+    square: square_burnt,
+    unlockable: unlockable_burnt,
+    title: 'burnt',
   },
   {
     player: "alpha-test",
     imageUrl: "https://ipfs.io/ipfs/QmYRBDjBixJczcXs6fNunG9EGPsHqQT62ifEBzYTS1SjGe/?creator=tz1cpiv1qgjzNsMbqHYyUdH8XzZ672bjdm2E&objkt=612561&viewer=",
-    square: 'square_alpha_test',
-    unlockable: 'unlockable_alpha_test',
+    square: square_alpha_test,
+    unlockable: unlockable_alpha_test,
+    title: 'acid beach'
   },
   {
     player: "dapp",
     imageUrl: "https://hic-af.infura-ipfs.io/ipfs/QmSHtBDT86HBpZTpTGbbEUVQcjGzmrQyW2RbNaXTTqDxYu?creator=tz1cpiv1qgjzNsMbqHYyUdH8XzZ672bjdm2E&objkt=612561&viewer=",
-    square: 'square_boxxed',
-    unlockable: 'unlockable_boxxed',
+    square: square_boxxed,
+    unlockable: unlockable_boxxed,
+    title: 'boxxed'
   }
 ];
 
@@ -81,56 +94,58 @@ const Dashboard = ({
   return (
     <div className="bg-black">
       {isload? 
-        <img src={load} alt="load" className="loading" />:
-    <div className="max-w-5xl mx-auto  py-12">
-      <div className="h-screen">
-        <div className="bg-white py-2 flex items-center px-12 justify-between mx-4 md:mx-0">
-          <div className="flex items-center space-x-8">
-            <ul className="flex space-x-10 text-white">
-            <img src={et_new_logo} alt="logo" className="w-16 h-10"/>
-            </ul>
+      <div className="w-full h-screen flex justify-center items-center">
+        <img src={load} alt="load" className="w-80 h-80"/>
+      </div>:
+        <div className="max-w-5xl mx-auto  py-12">
+          <div className="h-screen">
+            <div className="bg-white py-2 flex items-center px-12 justify-between mx-4 md:mx-0">
+              <div className="flex items-center space-x-8">
+                <ul className="flex space-x-10 text-white">
+                <img src={et_new_logo} alt="logo" className="w-16 h-10"/>
+                </ul>
+              </div>
+              <ConnectButton
+                Tezos={Tezos}
+                setContract={setContract}
+                setWallet={setWallet}
+                setUserAddress={setUserAddress}
+                setUserBalance={setUserBalance}
+                setStorage={setStorage}
+                userAddress={userAddress}
+                beaconConnection={beaconConnection}
+                setBeaconConnection={setBeaconConnection}
+                wallet={wallet}
+              />
+            </div>
+            <iframe src={`${result?.imageUrl}${userAddress}`} className="w-full h-iframe mx-4 md:mx-0"/>
+            <div className="bg-white flex justify-center py-4 mx-4 md:mx-0">
+              <button className="bg-black text-white font-bold rounded px-8 py-2">Collect</button>
+            </div>
           </div>
-          <ConnectButton
-            Tezos={Tezos}
-            setContract={setContract}
-            setWallet={setWallet}
-            setUserAddress={setUserAddress}
-            setUserBalance={setUserBalance}
-            setStorage={setStorage}
-            userAddress={userAddress}
-            beaconConnection={beaconConnection}
-            setBeaconConnection={setBeaconConnection}
-            wallet={wallet}
-          />
+          <section className="text-white grid grid-cols-2 md:grid-cols-4 gap-4 font-mathias mb-16">
+            <div className="text-center space-y-3 font-bold">
+              <div className="text-5xl">1.5</div>
+              <div>PRICE</div>
+            </div>
+            <div className="text-center space-y-3 font-bold">
+              <div className="text-5xl">75</div>
+              <div>EDITIONS</div>
+            </div>
+            <div className="text-center space-y-3 font-bold">
+              <div className="text-5xl">55</div>
+              <div>COLLECTORS</div>
+            </div>
+            <div className="text-center space-y-3 font-bold">
+              <div className="text-5xl">112</div>
+              <div>VOLUME</div>
+            </div>
+          </section>
+          <Content square={result?.square} unlockable={result?.unlockable} title={result?.title}/>
+          <Collection/>
+          <Miscellaneous/>
+          <Footer/>
         </div>
-        <iframe src={`${result?.imageUrl}${userAddress}`} className="w-full h-iframe mx-4 md:mx-0"/>
-        <div className="bg-white flex justify-center py-4 mx-4 md:mx-0">
-          <button className="bg-black text-white font-bold rounded px-8 py-2">Collect</button>
-        </div>
-      </div>
-      <section className="text-white grid grid-cols-2 md:grid-cols-4 gap-4 font-mathias mb-16">
-        <div className="text-center space-y-3 font-bold">
-          <div className="text-5xl">1.5</div>
-          <div>PRICE</div>
-        </div>
-        <div className="text-center space-y-3 font-bold">
-          <div className="text-5xl">75</div>
-          <div>EDITIONS</div>
-        </div>
-        <div className="text-center space-y-3 font-bold">
-          <div className="text-5xl">55</div>
-          <div>COLLECTORS</div>
-        </div>
-        <div className="text-center space-y-3 font-bold">
-          <div className="text-5xl">112</div>
-          <div>VOLUME</div>
-        </div>
-      </section>
-      <Content square={result?.square} unlockable={result?.unlockable}/>
-      <Collection/>
-      <Miscellaneous/>
-      <Footer/>
-    </div>
       }
     </div>
   );
