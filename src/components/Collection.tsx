ec2e4059aaf777796a44c8ef4baea250f959f22c
bryanballidev <bryanballidev@gmail.com>
2022-10-28 12:49:42 -0500

    troubleshooting play button

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import QmRft7BBYvJLdVUmYRw9stoUAaomtvzxUL8n5JLqTCSMWj from "../assets/QmRft7BBYvJLdVUmYRw9stoUAaomtvzxUL8n5JLqTCSMWj.jpeg";
 import QmX6mLXAv3TgBBsP5Ahjiz9R3zV3sid2DnXQLsjNuo1vJ1 from "../assets/QmX6mLXAv3TgBBsP5Ahjiz9R3zV3sid2DnXQLsjNuo1vJ1.jpeg";
 import QmPgdPxBpeMaWpUzjxxV4boeB9P4nC63zApn98TSn7MPoX from "../assets/QmPgdPxBpeMaWpUzjxxV4boeB9P4nC63zApn98TSn7MPoX.jpeg";
+import founder from "../assets/founder.jpg";
 // import QmQDUgmJdguCMhK9NVvqWdYpTnXjDDZhmA5E7Di2U6BgVJ from "../assets/QmQDUgmJdguCMhK9NVvqWdYpTnXjDDZhmA5E7Di2U6BgVJ.jpeg";
 import icon0 from "../assets/icon0.png";
 import icon1 from "../assets/icon1.png";
@@ -67,13 +68,111 @@ const Collection = () => {
 
     <section className="bg-black m-0 mb-12 sm:mt-0 md:mx-0 items-center flex flex-col justify-center pl-4 pr-4">
 
-     <div className="bg-black mx-0 mb-4 w-auto rounded-sm px-0">
-      <div className="font-mathias mt-14 mb-0 mx-6 text-center text-yellow-75 text-5xl">
+      
+
+<div className="font-mathias mt-24 mb-0 mx-6 text-center text-yellow-75 text-5xl sm:text-7xl">
         GENESIS COLLECTION
       </div>
 
-      <dt className="mt-2 text-sm text-gray-500 p-4 text-justify ">
-        Bai-ee is a House Dj/producer, Live Performance Act and Record Label Owner based in Chicago. His Genesis Collection consists of 4 original tracks available for the first time as collectible music. Customize each track fro your own performance within within these interactive Music OBJKTs. </dt>
+
+<div className="relative max-w-lg mx-auto md:max-w-2xl sm:mt-12 min-w-0 break-words gap-24 bg-black w-full mb-6 shadow-lg rounded-xl mt-4 text-yellow-75 flex flex-row">
+
+      <div className="px-0 flex flex-col sm:flex-row">
+
+        <div className="flex flex-wrap justify-center flex-col">
+          
+          <div className="mr-auto ml-auto mt-0 mb-0 ml-0 gap-2 flex w-auto flex-col mx-0 h-auto items-center">
+
+            <img src={founder} alt="founder_pic" className="rounded-full h-auto"/>
+
+            <div className="w-full text-center mt-0 opacity-0 h-0">
+
+                <div className="flex justify-center lg:pt-4 pt-8 pb-0">
+                    <div className="p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-slate-700">3,360</span>
+                        <span className="text-sm text-slate-400">Photos</span>
+                    </div>
+                    <div className="p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-slate-700">2,454</span>
+                        <span className="text-sm text-slate-400">Followers</span>
+                    </div>
+                    <div className="p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-slate-700">564</span>
+                        <span className="text-sm text-slate-400">Following</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+      </div>
+
+      <div className="mr-auto mt-0 mb-0 ml-0 gap-2 flex flex-col mx-0 h-auto items-center">
+
+        <div className="w-full px-4">
+
+          <div className="pl-0 gap-0 flex flex-row items-center">
+
+            <div className="text-center sm:text-left mt-0 w-full">
+              <h3 className="font-mathias text-5xl text-slate-700 font-bold leading-normal -mb-4 text-center sm:text-left">BAI-EE</h3>
+              <div className="text-xs mt-0 mb-2 text-slate-400 font-bold uppercase mt-2">
+                  {/* <i className="font-mathias fas text-left mb-0 fa-map-marker-alt mr-0 text-slate-400 opacity-75"></i> */}
+                  Chicago, Il
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+
+
+
+
+    <p className="font-light leading-relaxed text-slate-600 mb-4 text-justify text-xs ">   Bai-ee is a House Dj/producer, Live Performance Act and Record Label Owner based in Chicago. His Genesis Collection consists of 4 original tracks available for the first time as collectible music. Customize each track fro your own performance within within these interactive Music OBJKTs.</p>
+
+
+    <div className="pl-0 gap-1 flex flex-row">
+              <a href="https://objkt.com/asset/hicetnunc/612561"  target="_blank" rel="noreferrer" className="font-mathias bg-yellow-75 w-auto text-center px-4 py-1 text-black text-xs rounded-md hover:opacity-50">
+                Website
+              </a>
+              <a href="https://objkt.com/asset/hicetnunc/612561"  target="_blank" rel="noreferrer" className="font-mathias bg-yellow-75 w-auto text-center px-4 py-1 text-black text-xs rounded-md hover:opacity-50">
+                Twitter
+              </a>
+              <a href="https://objkt.com/asset/hicetnunc/612561"  target="_blank" rel="noreferrer" className="font-mathias bg-yellow-75  w-auto text-center px-4  py-1 text-black text-xs rounded-md hover:opacity-50">
+                Discogs
+              </a>
+            </div>
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+      </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+     <div className="bg-black mx-0 mb-4 w-auto rounded-sm px-0 mt-8">
+
+
+
+
+
 
       {/* Players */}
       {/* <div className="grid md:grid-cols-3 gap-x-6 lg:gap-x-12"> */}
@@ -83,7 +182,7 @@ const Collection = () => {
 
       <div className="mb-0 lg:mb-0 border-5 border-yellow-75 ">
       {/* <div className="bg-white block rounded-lg shadow-lg">  */}
-        <div className="text-yellow-75 h-24 sm:h-28 md:h-96 border-0 border-yellow-75 flex flex-row rounded-md md:flex-col items-center font-mathias justify-evenly py-0 px-0 md:p-4 bg-transparent player1">
+        <div className="text-yellow-75 h-24 sm:h-28 md:h-96 border-2 border-yellow-75 flex flex-row rounded-lg md:flex-col items-center font-mathias justify-evenly py-0 px-0 md:p-4 bg-transparent player1">
           {/* <img alt="icon0" className="w-full h-auto"/> */}
           <img src={et01} alt="icon1" className="w-20 md:w-auto md:h-3/4 p-0 ml-1 sm:ml-10 md:ml-0 shadow-2xl rounded-md"/>
 
